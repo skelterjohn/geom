3a0540e0ff7250af240bbeaa4ee197eb5847f5cc
John Asmuth <jasmuth@google.com>
2013-04-16 08:45:13 -0400

    added license pointer to each source file

--- point_test.go
+++ point_test.go
@@ -1,6 +1,6 @@
-// Copyright 2009 The geom Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// Copyright 2012 The geom Authors. All rights reserved.
+// Use of this source code is governed by a license that
+// can be found in the LICENSE file.
 
 package geom
 
